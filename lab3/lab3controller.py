de3704715c9dcd0693ae688972a4ab596064a016
elijahc <ejcordov@ucsc.edu>
2018-03-01 14:54:50 -0800

    first push

---
+++ lab3/lab3controller.py
@@ -0,0 +1,47 @@
+# Lab 3 Skeleton
+#
+# Based on of_tutorial by James McCauley
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+log = core.getLogger()
+
+class Firewall (object):
+  """
+  A Firewall object is created for each switch that connects.
+  A Connection object for that switch is passed to the __init__ function.
+  """
+  def __init__ (self, connection):
+    # Keep track of the connection to the switch so that we can
+    # send it messages!
+    self.connection = connection
+
+    # This binds our PacketIn event listener
+    connection.addListeners(self)
+
+  def do_firewall (self, packet, packet_in):
+    # The code in here will be executed for every packet.
+    print "Example Code."
+
+  def _handle_PacketIn (self, event):
+    """
+    Handles packet in messages from the switch.
+    """
+
+    packet = event.parsed # This is the parsed packet data.
+    if not packet.parsed:
+      log.warning("Ignoring incomplete packet")
+      return
+
+    packet_in = event.ofp # The actual ofp_packet_in message.
+    self.do_firewall(packet, packet_in)
+
+def launch ():
+  """
+  Starts the component
+  """
+  def start_switch (event):
+    log.debug("Controlling %s" % (event.connection,))
+    Firewall(event.connection)
+  core.openflow.addListenerByName("ConnectionUp", start_switch)
