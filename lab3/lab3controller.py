ca43ff447495d3171cf35484c2cb658d09306dd9
zerotsu <popuko@cock.li>
2018-03-04 20:40:39 -0800

    hi

--- lab3/lab3controller.py
+++ lab3/lab3controller.py
@@ -6,6 +6,8 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 
 log = core.getLogger()
+valid = {}
+firewallOk = {}
 
 class Firewall (object):
   """
@@ -19,23 +21,58 @@ class Firewall (object):
 
     # This binds our PacketIn event listener
     connection.addListeners(self)
+  
+  # send packet
+  def send(self,event,dst_port = of.OFPP_ALL):
+    msg = of.ofp_packet_out(in_port=event.ofp.in_port)
+    if event.ofp.buffer_id is not None and event.ofp.buffer_id != -1:
+       msg.buffer_id = event.ofp.buffer_id
+    else:
+      if event.ofp.data:
+        return
+    msg.actions.append(of.ofp_action_output(port = dst_port))
+    event.connection.send(msg)
+
 
   def do_firewall (self, packet, packet_in):
     # The code in here will be executed for every packet.
-    print "Example Code."
+    print ""
 
   def _handle_PacketIn (self, event):
     """
     Handles packet in messages from the switch.
     """
-
     packet = event.parsed # This is the parsed packet data.
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
-
     packet_in = event.ofp # The actual ofp_packet_in message.
     self.do_firewall(packet, packet_in)
+    tcpp = event.parsed.find('tcp')
+    arpp = event.parsed.find('arp')
+    if tcpp is None and arpp is None:
+      print "wat"
+      return 
+    # build flow both ways
+    valid[(event.connection,packet.src)] = event.port
+    destination = valid.get((event.connection,packet.dst))
+    if destination is None:
+      self.send(event, of.OFPP_ALL)
+    else:
+      msg = of.ofp_flow_mod()
+      msg.idle_timeout = 10
+      msg.hard_timeout = 30     
+      if tcpp is not None: 
+        msg.match.dl_type = 0x800
+        msg.match.nw_proto = 6
+        print "lololol"
+      else:  
+        msg.match.dl_type = 0x806
+        msg.match.nw_proto = None 
+      msg.match.dl_src = packet.src
+      msg.match.dl_dst = packet.dst
+      msg.actions.append(of.ofp_action_output(port = destination))
+      event.connection.send(msg) 
 
 def launch ():
   """
