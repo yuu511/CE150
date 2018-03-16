54bcb20c9de4eb92ea60f3a6821bd24cef048041
elijahc <ejcordov@ucsc.edu>
2018-03-16 01:17:03 -0700

    auto

--- lab4/finalcontroller_skel.py
+++ lab4/finalcontroller_skel.py
@@ -109,7 +109,7 @@ class Final (object):
          print port_on_switch
          print switch_id 
          self.installFlow(ip_packet.srcip,ip_packet.dstip,port_on_switch,switch_id,0x800,6)
-         self.installFlow(ip_packet.dstip,ip_packet.srcip,switch_id,port_on_switch,0x800,6)
+         self.installFlow(ip_packet.dstip,ip_packet.srcip,port_on_switch,switch_id,0x800,6)
     print "LUL"
 
   def _handle_PacketIn (self, event):
