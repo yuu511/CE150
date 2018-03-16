c3622c8d3abaab54e94cb0fb5a3f4540fc89b42e
elijahc <ejcordov@ucsc.edu>
2018-03-16 13:29:22 -0700

    auto

--- lab4/finalcontroller_skel.py
+++ lab4/finalcontroller_skel.py
@@ -104,12 +104,12 @@ class Final (object):
        ip_packet = packet.payload
        if ip_packet.protocol == ip_packet.TCP_PROTOCOL:
          tcp_packet = ip_packet.payload
-         print port_on_switch
-         print switch_id
          if (switch_id == 1):
+           print "SWITCH ID 1"
            self.installFlow(ip_packet.srcip,ip_packet.dstip,port_on_switch,1,0x800,6)
            self.installFlow(ip_packet.dstip,ip_packet.srcip,1,port_on_switch,0x800,6)
          if (switch_id == 4): 
+           print "SWITCH ID 4"
            self.installFlow(ip_packet.srcip,ip_packet.dstip,port_on_switch,5,0x800,6)
            self.installFlow(ip_packet.dstip,ip_packet.srcip,5,port_on_switch,0x800,6)
        if ip_packet.protocol == ip_packet.ICMP_PROTOCOL:
