5b48703d6c61d30048497cda3cf2efe953935641
elijahc <ejcordov@ucsc.edu>
2018-03-16 17:09:08 -0700

    auto

--- lab4/finalcontroller_skel.py
+++ lab4/finalcontroller_skel.py
@@ -105,13 +105,23 @@ class Final (object):
 
        if ip_packet.protocol == ip_packet.TCP_PROTOCOL:
          tcp_packet = ip_packet.payload
-         if (switch_id != 4):
-           if (ip_packet.dstip == '123.45.67.89'):
+          
+         if (ip_packet.dstip == '123.45.67.89'):
              print "STOPPU"
+             return
+
+         if (switch_id != 4):
+           print "aga aga"
+           if (ip_packet.dstip == '123.45.67.89'):
+             print "herro"
+             return
            self.installFlow(ip_packet.srcip,ip_packet.dstip,tcp_packet.srcport,tcp_packet.dstport,0x800,6)
            self.installFlow(ip_packet.dstip,ip_packet.srcip,tcp_packet.dstport,tcp_packet.srcport,0x800,6)
            Final.resend (self,packet,1)  
          if (switch_id == 4): 
+           if (ip_packet.dstip == '123.45.67.89'):
+             print "Naisu"
+             return
            self.installFlow(ip_packet.srcip,ip_packet.dstip,tcp_packet.srcport,tcp_packet.dstport,0x800,6)
            self.installFlow(ip_packet.dstip,ip_packet.srcip,tcp_packet.dstport,tcp_packet.srcport,0x800,6)
            if (ip_packet.dstip == '10.1.1.10'):
@@ -124,10 +134,6 @@ class Final (object):
              Final.resend (self,packet,4)
            if (ip_packet.dstip == '10.5.5.50'):
              Final.resend (self,packet,5)
-           if (ip_packet.dstip == '123.45.67.89'):
-             print "Naisu"
-             return
-             Final.resend (self,packet,6)
 
 
        if ip_packet.protocol == ip_packet.UDP_PROTOCOL:
